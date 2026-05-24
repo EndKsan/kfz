58fb872d76673541027e9fe458c2b3736c301379
End_ksan <endreus.kenny.figueiro@gmail.com>
2026-05-24 18:07:13 -0300

    update

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -14,7 +14,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/layout.css') }}">
+	 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 	<title>Layout</title>
 </head>
